7def9cbb46b9a7bc4486884f6a89aa4e0d6e05a7
VaniushaR <vaniusha.ra@gmail.com>
2018-08-11 12:39:54 -0500

    Agregando Firebase

---
+++ src/data.js
@@ -0,0 +1,14 @@
+//firebase
+src="https://www.gstatic.com/firebasejs/5.3.1/firebase.js">
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAiAsGQvnKAXSnwwTQJV71ua8V2uE9EM00",
+    authDomain: "ux-kanban-citybanamex.firebaseapp.com",
+    databaseURL: "https://ux-kanban-citybanamex.firebaseio.com",
+    projectId: "ux-kanban-citybanamex",
+    storageBucket: "ux-kanban-citybanamex.appspot.com",
+    messagingSenderId: "357015167261"
+  };
+  firebase.initializeApp(config);
+
+
